e73b75e9554c5934d6a1944ee7b04ef6bf037cf4
dana <dana.gomez@alumnos.udg.mx>
2018-10-31 19:31:49 -0600

    actualizacion de servidor

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
+import {Http, HttpModule} from "@angular/http";
 
 /**
  * Generated class for the LoginPage page.
@@ -18,7 +19,10 @@ export class LoginPage {
 
   home = HomePage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  usuario = '';
+  password = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   }
 
   ionViewDidLoad() {
@@ -26,11 +30,20 @@ export class LoginPage {
   }
 
   clickHome(){
-    ///console.log("clickHome");
+    console.log("clickHome");
+    console.log(this.usuario);
+    console.log(this.password);
 
+    this.http.get('/home/')
+      .subscribe( data => {
+        console.log(data.text());
+      }, error => {
+          console.log("error");
+        }
+      );
     ///this.navCtrl.push(this.home);
 
-    this.navCtrl.setRoot(this.home);
+    ///this.navCtrl.setRoot(this.home);
   }
 }
 
